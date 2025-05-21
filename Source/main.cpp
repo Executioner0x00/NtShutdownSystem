7a3ac65e8688ad6def34cbb6c21c426fa722da8c
Executioner <diamondintheruffvendorevents@outlook.com>
2025-05-21 15:07:33 -0400

    Added example

---
+++ Source/main.cpp
@@ -0,0 +1,29 @@
+#include <windows.h>
+#include <winternl.h>
+typedef NTSTATUS(NTAPI* RtlAdjustPrivilege)(ULONG Privilege, BOOLEAN Enable, BOOLEAN CurrentThread, PBOOLEAN PreviousValue);
+typedef NTSTATUS(NTAPI* NtShutdownSystem)(IN SHUTDOWN_ACTION Action);
+
+INT main() {
+	HMODULE hNtdll = LoadLibraryW(L"ntdll.dll");
+	if (!hNtdll) {
+		return 0x01;
+	}
+
+	RtlAdjustPrivilege pRtlAdjustPrivilege = (RtlAdjustPrivilege)GetProcAddress(hNtdll, "RtlAdjustPrivilege");
+	NtShutdownSystem pNtShutdownSystem = (NtShutdownSystem)GetProcAddress(hNtdll, "NtShutdownSystem");
+
+	if (!pRtlAdjustPrivilege || !pNtShutdownSystem) {
+		FreeLibrary(hNtdll);
+		return 0x01;
+	}
+
+	BOOLEAN previousValue;
+	NTSTATUS status = pRtlAdjustPrivilege(19, TRUE, FALSE, &previousValue);
+
+	if (status == 0) {
+		NtShutdownSystem(2);
+	}
+
+	FreeLibrary(hNtdll);
+	return 0x00;
+}
